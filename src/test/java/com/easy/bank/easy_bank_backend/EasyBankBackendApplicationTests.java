7c542d8fd10ab0d3cb5c5292ca1f5d9bed5df888
zine-b <zineb.marricha99@gmail.com>
2026-06-17 16:31:49 +0200

    init app

---
+++ src/test/java/com/easy/bank/easy_bank_backend/EasyBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.bank.easy_bank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
